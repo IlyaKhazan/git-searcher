54a808f912b244f89feca7a8dc717872f3e0d3cd
Ilya Khazan <p9067700518@gmail.com>
2022-07-02 16:27:11 +0300

    added isFetching state to reposReducer

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRepos } from '../actions/repos';
 
@@ -9,6 +9,13 @@ import Repo from '../Repo/Repo';
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos.items);
+  const isFetching = useSelector((state) => state.repos.isFetching);
+
+  const [searchValue, setSearchValue] = useState('');
+
+  const searchHandler = () => {
+    dispatch(getRepos(searchValue));
+  };
 
   useEffect(() => {
     dispatch(getRepos());
@@ -16,10 +23,25 @@ const Main = () => {
 
   return (
     <div>
-      Main
-      {repos.map((repo) => (
-        <Repo repo={repo} />
-      ))}
+      <div className={styles.search}>
+        <input
+          value={searchValue}
+          onChange={(evt) => setSearchValue(evt.target.value)}
+          type="text"
+        />
+        <button className={styles.searchBtn} onClick={() => searchHandler()}>
+          Поиск
+        </button>
+      </div>
+      {isFetching ? (
+        <div className={styles.fetching}>The data is fetching</div>
+      ) : (
+        <div>
+          {repos.map((repo) => (
+            <Repo key={repo.id} repo={repo} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
